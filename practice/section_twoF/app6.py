8331c718eb0860db73be5db2feed242292219e5f
seung_ho_choi.s <chltmdgh517@naver.com>
2024-05-16 21:48:26 +0900

    인스타그램 봇 만들기(자동 로그인)

--- practice/section_twoF/app6.py
+++ practice/section_twoF/app6.py
@@ -8,12 +8,16 @@ import time
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
-
 driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://www.instagram.com/')
 
-time.sleep(2) #창이 다 뜰때가지 기다려야됨
+time.sleep(2)  # 창이 다 뜰때가지 기다려야됨
+#
+# element = driver.find_element(By.CSS_SELECTOR,'input[name="username"]') # 요소 찾기는 이렇게 아이디는 # 클래스는. name은 바로 앞에
+# print(element.text)
 
-element = driver.find_element(By.CSS_SELECTOR,'._ab37') # 요소 찾기는 이렇게 아이디는 #
-
-print(element.text)
+e = driver.find_element(By.CSS_SELECTOR, 'input[name="username"]')
+e.send_keys('csh_crawling_test')
+e = driver.find_element(By.CSS_SELECTOR, 'input[name="password"]')
+e.send_keys("qkek@@0312")
+e.send_keys(Keys.ENTER) #로그인
