7bb4d12effab58cbe9134fa0cb51a665ffa50762
seung_ho_choi.s <chltmdgh517@naver.com>
2024-05-16 22:48:07 +0900

    인스타그램 봇 만들기(페이지 이동과 이미지 수집)

--- practice/section_twoF/app6.py
+++ practice/section_twoF/app6.py
@@ -1,3 +1,4 @@
+import urllib.request
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -20,4 +21,20 @@ e = driver.find_element(By.CSS_SELECTOR, 'input[name="username"]')
 e.send_keys('csh_crawling_test')
 e = driver.find_element(By.CSS_SELECTOR, 'input[name="password"]')
 e.send_keys("qkek@@0312")
-e.send_keys(Keys.ENTER) #로그인
+e.send_keys(Keys.ENTER)  # 로그인
+
+time.sleep(4)
+# 페이지 이동
+driver.get("https://www.instagram.com/explore/tags/%EC%BD%94%EB%82%9C/")
+driver.implicitly_wait(10)  # 아래 요소가 없으면 10초간 기다리자
+driver.find_element(By.CSS_SELECTOR, '._aagw').click()  # 이 클래스가 많음 근데 element 쓰면 맨 위에꺼 하나만 찾아주고 elements면 전부 찾아서 리스트에 담아줌
+
+time.sleep(10)
+# 사진 저장
+img_url = driver.find_element(By.CSS_SELECTOR, '.x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3').get_attribute('src')
+if img_url:
+    print("이미지 URL:", img_url)
+    urllib.request.urlretrieve(img_url, '1.jpg')
+    print("이미지 다운로드 완료")
+else:
+    print("이미지 URL이 없습니다.")
